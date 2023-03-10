5a00e9a56d8c3eac2233e310d80539949ab918b7
Shashwat <shashwat.bhandari.14@gmail.com>
2023-03-10 18:23:58 +0800

    Add input validation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Choose: [1] for addition");
+        System.out.println("        [2] for subtraction");
+        System.out.println("        [3] for multiplication");
+        System.out.println("        [4] for division");
+        System.out.println("        [5] to quit");
+
+        int option = 0;
+        do {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print(">>> ");
+            if (validate_option(scanner)) {
+                option = scanner.nextInt();
+                if (option != 1 && option != 2 && option != 3 && option != 4 && option != 5) {
+                    System.out.println("Error - please choose a valid option");
+                }
+            }
+            else {
+                System.out.println("Error - please provide valid input");
+            }
+
+        } while (option != 1 && option != 2 && option != 3 && option != 4 && option != 5);
+    }
+
+    public static int generate_num(int start, int range) {
+        return (int) (Math.random() * range) + start;
+    }
+
+    public static boolean validate_option(Scanner check) {
+        return check.hasNextInt();
     }
 }
