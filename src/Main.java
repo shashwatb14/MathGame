ef9dcf0eed975ca018001960878f79b36eaae564
Shashwat <shashwat.bhandari.14@gmail.com>
2023-03-11 15:49:56 +0800

    Update play_addition method

--- src/Main.java
+++ src/Main.java
@@ -21,11 +21,11 @@ public class Main {
         // input validation
         int option = 0;
         do {
-            Scanner scanner = new Scanner(System.in);
+            Scanner getin = new Scanner(System.in);
             System.out.print(">>> ");
             // first check if it is integer
-            if (scanner.hasNextInt()) {
-                option = scanner.nextInt();
+            if (getin.hasNextInt()) {
+                option = getin.nextInt();
                 // check if it is a valid choice
                 if (option < 1 || option > 5) {
                     System.out.println("Error - please choose a valid option");
@@ -33,17 +33,49 @@ public class Main {
             }
             else System.out.println("Error - please provide valid input");
         } while (option < 1 || option > 5);
+
+        if (option == 1) {
+            while (true) {
+                play_addition(4);
+            }
+        }
     }
 
-    // returns random number from a specific range
-    // doesn't include the last number
+    /*
+     * returns random number from a specific range
+     * doesn't include the last number
+     */
     public static int generate_num(int start, int range) {
         return (int) (Math.random() * range) + start;
     }
 
     // todo - returns number of points scored
     public static int play_addition(int level) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner getin;
+        int[] numbers = new int[level];
+
+        // generate numbers based on level
+        int start = (int) Math.pow(10, level);
+        int range = (int) Math.pow(10, level + 1) - start;
+        int min = generate_num(start, range);
+        int max = generate_num(start, range);
+
+        for (int i = 0; i < 10000000; i++) {
+            // numbers[i] = generate_num(start, range);
+            int x = generate_num(start, range);
+            if (x < min) min = x;
+            if (x > max) max = x;
+        }
+        System.out.println("min = " + min + "\nmax = " + max);
+
+        int answer;
+        do {
+            getin = new Scanner(System.in);
+            System.out.print("> ");
+            // first check if it is integer
+            if (getin.hasNextInt()) answer = getin.nextInt();
+            else System.out.println("Error - please provide valid input");
+        } while (!getin.hasNextInt());
         return 0;
     }
 }
