f0b63cc7805681342e66ade0a971055d6e440b44
Shashwat <shashwat.bhandari.14@gmail.com>
2023-03-15 15:32:17 +0800

    Add random operations

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
                     "[2] for subtraction",
                     "[3] for multiplication",
                     "[4] for division",
-                    "[5] for mixed operations",
+                    "[5] for random operations",
                     "[6] to quit"
             };
 
@@ -43,7 +43,8 @@ public class Main {
                 if (option == 1) play(level, '+');
                 else if (option == 2) play(level, '-');
                 else if (option == 3) play(level, '*');
-                else play(level, '/');
+                else if (option == 4) play(level, '/');
+                else play(level, 'r');
             }
         }
         System.out.println("\nYour final score: " + score);
@@ -55,6 +56,86 @@ public class Main {
      */
     public static int generate_num(int start, int range) {
         return (int) (Math.random() * range) + start;
+    }
+
+    // allow for custom difficulty level
+    public static int difficulty() {
+        System.out.println("Choose the difficulty level (1 - 5): ");
+        int level = 0;
+        do {
+            Scanner getin = new Scanner(System.in);
+            System.out.print(">>> ");
+            // first check if it is integer
+            if (getin.hasNextInt()) {
+                level = getin.nextInt();
+                // check if it is within a valid range
+                if (level < 1 || level > 5) {
+                    System.out.println("Error - difficulty out of range (1 - 5)");
+                }
+            } else System.out.println("Error - please provide valid input");
+        } while (level < 1 || level > 5);
+        return level;
+    }
+
+    // captures timing and updates score
+    public static void play(int level, char operator) {
+        while (true) {
+            int result;
+
+            // capture initial timing
+            Date d1 = new Date();
+            double start = d1.getTime();
+
+            if (operator == '+' || operator == '-') result = arithmetic(level, operator);
+            else if (operator == '*') result = multiplication(level);
+            else if (operator == '/') result = division(level);
+            else result = random(level);
+
+            // capture final timing
+            Date d2 = new Date();
+            double end = d2.getTime();
+
+            // rogue value
+            if (result == -6) break;
+            score += result;
+            System.out.printf("Score: %6d\n", score);
+            System.out.println("\nTime taken: ~" + ((end - start) / 1000) + "s");
+        }
+    }
+
+    // validate and check input
+    public static int check(int result, int level) {
+        Scanner getin;
+        int answer;
+        do {
+            getin = new Scanner(System.in);
+            System.out.print("> ");
+            // first check if it is integer
+            if (getin.hasNextInt()) {
+                answer = getin.nextInt();
+                break;
+            }
+            // rogue value
+            else if (getin.hasNext("q") || getin.hasNext("exit")) return -6;
+            else {
+                System.out.println("Error - please provide valid input");
+                System.out.println("Tip: type 'q' or \"exit\" return to the menu.");
+            }
+        } while (true);
+
+        // check answer and award points according to level
+        if (answer == result) {
+            int points = generate_num((level - 1) * 10 + 1, level * 10);
+            System.out.printf("\nCorrect! You got %d point(s)!\n", points);
+            return points;
+        } else {
+            // loss points are half the range of win points
+            int loss = generate_num((((level - 1) * 10) / 2) + 1, (level * 10) / 2);
+            // rogue value
+            if (loss == -6) loss = generate_num((((level - 1) * 10) / 2) + 1, (level * 10) / 2);
+            System.out.printf("\nWrong! The answer was %d, you lost %d point(s).\n", result, loss);
+            return -loss;
+        }
     }
 
     // returns number of points scored in arithmetic (only addition and subtraction)
@@ -113,7 +194,7 @@ public class Main {
         int num1 = generate_num(start, range), num2 = generate_num(start, range);
 
         // generate until it's perfectly divisible
-        while (num1 % num2 != 0 && num1 != num2) {
+        while (num1 % num2 != 0 || num1 == num2) {
             num1 = generate_num(start, range);
             num2 = generate_num(start, range);
         }
@@ -127,80 +208,55 @@ public class Main {
         return check(quotient, level);
     }
 
-    // allow for custom difficulty level
-    public static int difficulty() {
-        System.out.println("Choose the difficulty level (1 - 5): ");
-        int level = 0;
-        do {
-            Scanner getin = new Scanner(System.in);
-            System.out.print(">>> ");
-            // first check if it is integer
-            if (getin.hasNextInt()) {
-                level = getin.nextInt();
-                // check if it is within a valid range
-                if (level < 1 || level > 5) {
-                    System.out.println("Error - difficulty out of range (1 - 5)");
+    // method for mixed operations
+    public static int random(int level) {
+        int[] numbers = new int[generate_num(level + 1, level)];
+        char[] operations = {'+', '-', '/', '*'};
+        int result = 0;
+        int prev = 1;
+
+        for (int i = 0, n = numbers.length; i < n; i++) {
+            char operator = operations[generate_num(0, 4)];
+            if (operator == '+' || operator == '-' || i == 0) {
+                // generate numbers for arithmetic based on level
+                int start = (int) Math.pow(10, level);
+                int range = (int) Math.pow(10, level + 1) - start;
+
+                int num = generate_num(start, range);
+
+                if (i == 0) result = num;
+                else if (operator == '-') {
+                    System.out.print(" - ");
+                    result -= num;
+                } else {
+                    System.out.print(" + ");
+                    result += num;
                 }
-            } else System.out.println("Error - please provide valid input");
-        } while (level < 1 || level > 5);
-        return level;
-    }
+                System.out.print(num);
+                prev = num;
+            } else if (operator == '*') {
+                // generate numbers for multiplication based on level
+                int start = (int) Math.pow(10, level - 1);
+                int range = (int) Math.pow(10, level);
 
-    public static void play(int level, char operator) {
-        while (true) {
-            int result;
+                int num = generate_num(start, range);
+                System.out.print(" * " + num);
+                result *= num;
+                prev = num;
+            } else {
+                int start = (int) Math.pow(10, level);
+                int range = (int) Math.pow(10, level + 2) - start;
 
-            // capture initial timing
-            Date d1 = new Date();
-            double start = d1.getTime();
+                int num = generate_num(start, range);
 
-            if (operator == '+' || operator == '-') result = arithmetic(level, operator);
-            else if (operator == '*') result = multiplication(level);
-            else result = division(level);
+                while (prev % num != 0 || prev == num) num = generate_num(start, range);
 
-            // capture final timing
-            Date d2 = new Date();
-            double end = d2.getTime();
-
-            // rogue value
-            if (result == -6) break;
-            score += result;
-            System.out.printf("Score: %6d\n", score);
-            System.out.println("\nTime taken: ~" + ((end - start) / 1000) + "s");
-        }
-    }
-
-    public static int check(int result, int level) {
-        Scanner getin;
-        int answer;
-        do {
-            getin = new Scanner(System.in);
-            System.out.print("> ");
-            // first check if it is integer
-            if (getin.hasNextInt()) {
-                answer = getin.nextInt();
-                break;
+                System.out.print(" / " + num);
+                result /= num;
+                prev = num;
             }
-            // rogue value
-            else if (getin.hasNext("q") || getin.hasNext("exit")) return -6;
-            else {
-                System.out.println("Error - please provide valid input");
-                System.out.println("Tip: type 'q' or \"exit\" return to the menu.");
-            }
-        } while (true);
-
-        // check answer and award points according to level
-        if (answer == result) {
-            int points = generate_num((level - 1) * 10 + 1, level * 10);
-            System.out.printf("\nCorrect! You got %d point(s)!\n", points);
-            return points;
-        } else {
-            // loss points are half the range of win points
-            int loss = generate_num((((level - 1) * 10) / 2) + 1, (level * 10) / 2);
-            // rogue value
-            if (loss == -6) loss = generate_num((((level - 1) * 10) / 2) + 1, (level * 10) / 2);
-            System.out.printf("\nWrong! The answer was %d, you lost %d point(s).\n", result, loss);
-            return -loss;
         }
+        System.out.println();
+        return check(result, level);
     }
 }
